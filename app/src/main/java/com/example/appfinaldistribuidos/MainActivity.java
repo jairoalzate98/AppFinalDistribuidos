a9d86a559ad2cee960e0a9b96c2dfd9b1e8b876c
manuel-celis <manuel.celis@uptc.edu.co>
2019-06-03 17:29:42 -0500

    Se valida si un usuario existe o no existe e igualmente de maneja el manejo de editText vacios

--- app/src/main/java/com/example/appfinaldistribuidos/MainActivity.java
+++ app/src/main/java/com/example/appfinaldistribuidos/MainActivity.java
@@ -2,17 +2,48 @@ package com.example.appfinaldistribuidos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import model.EstudianteManager;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText etCodigo;
+    private EditText etContrasenia;
+    private Toast toastMessage;
+    private EstudianteManager estudianteManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        estudianteManager = new EstudianteManager();
         etCodigo = findViewById(R.id.et_Codigo);
+        etContrasenia = findViewById(R.id.et_Contrasenia);
+    }
+
+    public void startApp(View view){
+        if (validateEmptyFields()){
+            if (estudianteManager.validateEstudiante(Integer.parseInt(etCodigo.getText().toString()), etContrasenia.getText().toString())){
+                toastMessage = Toast.makeText(this, "Usuario valido", Toast.LENGTH_SHORT);
+                toastMessage.show();
+            }else {
+                toastMessage = Toast.makeText(this, "Usuario no valido", Toast.LENGTH_SHORT);
+                toastMessage.show();
+            }
+        }
+    }
+
+    public boolean validateEmptyFields(){
+        if(TextUtils.isEmpty(etCodigo.getText().toString()) || TextUtils.isEmpty(etCodigo.getText().toString())){
+            toastMessage = Toast.makeText(this, "Por favor introduzca sus datos", Toast.LENGTH_LONG);
+            toastMessage.show();
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/model/EstudianteManager.java
+++ app/src/main/java/model/EstudianteManager.java
@@ -6,5 +6,18 @@ public class EstudianteManager {
 
     private ArrayList<Estudiante> estudianteList;
 
+    public EstudianteManager(){
+        estudianteList = new ArrayList<>();
+        estudianteList.add(new Estudiante(201611393, "a123", "Manuel"));
+    }
+
+    public boolean validateEstudiante(int id, String password){
+        for (Estudiante est: estudianteList) {
+            if (est.getId() == id && est.getPassword().equals(password)){
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
