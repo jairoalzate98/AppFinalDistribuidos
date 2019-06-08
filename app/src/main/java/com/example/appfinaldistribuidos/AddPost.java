7153cf774c19c1963b9c3619f59c0e9ea3c4e4db
manuel-celis <manuel.celis@uptc.edu.co>
2019-06-08 17:20:45 -0500

    Agregada la opcion de seleccionar la imagen para el post desde la camara, validado el maximo de caracteres por post

--- app/src/main/java/com/example/appfinaldistribuidos/AddPost.java
+++ app/src/main/java/com/example/appfinaldistribuidos/AddPost.java
@@ -1,13 +1,80 @@
 package com.example.appfinaldistribuidos;
 
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.os.Environment.getExternalStoragePublicDirectory;
 
 public class AddPost extends AppCompatActivity {
+
+    //TODO Agregar los componentes de la ventana de creacion de post, como agregar imagenes de la memoria?
+
+    private ImageView ivPhoto;
+    private String pathToFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_post);
+
+        ivPhoto = findViewById(R.id.iv_Photo);
+        if (Build.VERSION.SDK_INT >= 23){
+            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK){
+            if (requestCode == 1){
+                Bitmap bitmap = BitmapFactory.decodeFile(pathToFile);
+                ivPhoto.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+    public void takePhotoFromCamera(View view){
+        Intent takePhoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePhoto.resolveActivity(getPackageManager()) != null){
+            File photoFile = null;
+            photoFile = createPhotoFile();
+
+            if (photoFile != null) {
+                pathToFile = photoFile.getAbsolutePath();
+                Uri photoURI = FileProvider.getUriForFile(AddPost.this, "com.example.appfinaldistribuidos.fileprovider", photoFile);
+                takePhoto.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePhoto, 1);
+            }
+        }
+    }
+
+    private File createPhotoFile() {
+        String name = new SimpleDateFormat("yyyyMMDD_HHmmss").format(new Date());
+        File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = null;
+        try {
+            image = File.createTempFile(name, ".jpg", storageDir);
+        } catch (IOException e) {
+            Log.d("MyLog", "Except:" + e.toString());
+        }
+        return image;
     }
 }
